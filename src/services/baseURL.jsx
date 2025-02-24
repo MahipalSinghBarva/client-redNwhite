3c49f994744814913eb32606f2c0f9b1607d1508
Mahipal Singh <mahipalsingh450@gmail.com>
2025-02-24 13:22:39 +0530

    update baseURL

--- src/services/baseURL.jsx
+++ src/services/baseURL.jsx
@@ -1,2 +1,3 @@
-export const baseURL = "http://localhost:8080";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://server-rednwhite.onrender.com";
 export const token = localStorage.getItem("token");
